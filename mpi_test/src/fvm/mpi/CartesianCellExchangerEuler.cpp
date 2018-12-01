4d156afb3bab4c3edbc717c3121eccd7dc629fdf
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-12-01 23:07:42 +0100

    Fixed running without cuda

--- mpi_test/src/fvm/mpi/CartesianCellExchangerEuler.cpp
+++ mpi_test/src/fvm/mpi/CartesianCellExchangerEuler.cpp
@@ -3,12 +3,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -573,7 +573,7 @@ TEST_P(CartesianCellExchangerEulerTest, Test2D) {
 INSTANTIATE_TEST_CASE_P(CartesianCellExchangerEuler,
     CartesianCellExchangerEulerTest,
     ::testing::Values("cpu"
-
-        , "cuda"
-
+#ifdef ALSVINN_USE_CUDA
+    , "cuda"
+#endif
     ));
