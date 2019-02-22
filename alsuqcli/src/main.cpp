1d12b320fe097ef662df79760dfdb2d6fde21907
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-02-22 03:23:25 +0100

    fixes

--- alsuqcli/src/main.cpp
+++ alsuqcli/src/main.cpp
@@ -106,9 +106,44 @@ int main(int argc, char** argv) {
 
         }
 
-
+#ifdef ALSVINN_USE_MPI
+#ifdef ALSVINN_MPI_GPU_DIRECT
+        setenv("MPICH_RDMA_ENABLED_CUDA", "1", 1);
+        setenv("MV2_USE_CUDA", "1", 1);
+#endif
+#endif
         alsutils::mpi::setCudaDevice();
+#ifdef ALSSVINN_USE_MPI
         MPI_Init(&argc, &argv);
+#ifdef ALSVINN_MPI_GPU_DIRECT
+
+        // Just check that we can transfer on gpu
+
+        int rank, size;
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+        double* a;
+        cudaMalloc(&a, sizeof(a));
+
+        if (size > 1) {
+            MPI_Request requestSend;
+            MPI_Isend(a, 1, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD,
+                &requestSend);
+            MPI_Request requestReceive;
+            MPI_Irecv(a, 1, MPI_DOUBLE, (rank + 1) % size, 0, MPI_COMM_WORLD,
+                &requestReceive);
+            MPI_Wait(&requestSend, MPI_STATUS_IGNORE);
+            MPI_Wait(&requestReceive, MPI_STATUS_IGNORE);
+
+        }
+
+#endif
+#endif
+
+
+
+
 
         alsuq::mpi::ConfigurationPtr mpiConfig(new alsuq::mpi::Configuration(
                 MPI_COMM_WORLD));
