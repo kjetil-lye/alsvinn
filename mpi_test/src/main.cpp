d8c855bf17ecf89f93da64ef291ed5e3c0493d3d
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-02-22 02:45:04 +0100

    fixes

--- mpi_test/src/main.cpp
+++ mpi_test/src/main.cpp
@@ -3,12 +3,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,14 +22,15 @@
     #include <cuda_runtime.h>
 #endif
 #include <cstdlib>
+#include "alsutils/config.hpp"
 int main(int argc, char** argv) {
 #ifdef ALSVINN_USE_MPI
-#ifdef DALSVINN_HAS_GPU_DIRECT
+#ifdef ALSVINN_MPI_GPU_DIRECT
     setenv("MPICH_RDMA_ENABLED_CUDA", "1", 1);
     setenv("MV2_USE_CUDA", "1", 1);
 #endif
     MPI_Init(NULL, NULL);
-#ifdef DALSVINN_HAS_GPU_DIRECT
+#ifdef ALSVINN_MPI_GPU_DIRECT
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
